3643f7fbf496e50c71ad9145eab0b7d48123af46
MilojevicMarko <marko.milojevic026@gmail.com>
2023-11-18 21:32:25 +0100

    firstCommit-starter

---
+++ src/App.js
@@ -0,0 +1,16 @@
+const apiUrl = "http://localhost:5000/questions";
+
+function App() {
+	return (
+		<div className="quiz-container">
+			<h1>React Quiz</h1>
+			<div className="question-container">
+				<p>PITANJE</p>
+				<div className="options-container">PONUDJENI ODGOVORI</div>
+			</div>
+			<button>Next Question</button>
+		</div>
+	);
+}
+
+export default App;
