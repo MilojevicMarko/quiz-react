f7dbdb93839e0f3e0ce5fbb48ec35016c297100f
MilojevicMarko <marko.milojevic026@gmail.com>
2023-11-18 21:57:55 +0100

    second_commit_answer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
+
 const apiUrl = "http://localhost:5000/questions";
 
 function App() {
+	const [questions, setQuestions] = useState([]);
+
+	useEffect(() => {
+		fetch(apiUrl)
+			.then((res) => res.json())
+			.then((data) => setQuestions(data));
+	}, []);
+	console.log(questions);
+	if (questions.length === 0) {
+		return <h1>I have no data!</h1>;
+	}
 	return (
 		<div className="quiz-container">
 			<h1>React Quiz</h1>
 			<div className="question-container">
-				<p>PITANJE</p>
-				<div className="options-container">PONUDJENI ODGOVORI</div>
+				<p>{questions[0].question}</p>
+				<div className="options-container">
+					{questions[0].options.map((answer, index) => {
+						return (
+							<button type="Submit" key={index}>
+								{answer}
+							</button>
+						);
+					})}
+				</div>
 			</div>
 			<button>Next Question</button>
 		</div>
