acba0a71399310f3ecc262fa4b0a6f8a334a5757
MilojevicMarko <marko.milojevic026@gmail.com>
2023-11-19 23:12:39 +0100

    complete_correctAnswer_&_nextQuestion

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ const apiUrl = "http://localhost:5000/questions";
 
 function App() {
 	const [questions, setQuestions] = useState([]);
+	const [userAnswer, setUserAnswer] = useState(null);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [score, setScore] = useState(0);
 
 	useEffect(() => {
 		fetch(apiUrl)
 			.then((res) => res.json())
 			.then((data) => setQuestions(data));
 	}, []);
+
+	const handleAnswer = (index) => {
+		setUserAnswer(index);
+	};
+
+	//TODO NextQuestion
+	function handleNextQuestion() {
+		if (userAnswer === null) {
+			return;
+		}
+		setCurrentQuestion(currentQuestion + 1);
+
+		//TODO correct answer score
+		if (userAnswer === questions[currentQuestion].correctOption) {
+			setScore(score + questions[currentQuestion].points);
+		}
+		setUserAnswer(null);
+	}
+
 	console.log(questions);
 	if (questions.length === 0) {
 		return <h1>I have no data!</h1>;
@@ -18,18 +40,29 @@ function App() {
 		<div className="quiz-container">
 			<h1>React Quiz</h1>
 			<div className="question-container">
-				<p>{questions[0].question}</p>
+				<p>{questions[currentQuestion].question}</p>
+				<p>
+					Question {currentQuestion + 1} od {questions.length}
+				</p>
+				<p>Score : {score}</p>
 				<div className="options-container">
-					{questions[0].options.map((answer, index) => {
+					{questions[currentQuestion].options.map((answer, index) => {
 						return (
-							<button type="Submit" key={index}>
+							<button
+								type="Submit"
+								className={userAnswer === index ? "selected" : ""}
+								key={index}
+								onClick={() => {
+									handleAnswer(index);
+								}}
+								id={index}>
 								{answer}
 							</button>
 						);
 					})}
 				</div>
 			</div>
-			<button>Next Question</button>
+			<button onClick={handleNextQuestion}>Next Question</button>
 		</div>
 	);
 }
